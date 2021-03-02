3b83f5c08e455624c512dae6ba9e16709c8ef9ea
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-03-02 22:49:55 +0100

    loading completed

--- dataset.js
+++ dataset.js
@@ -1,4 +1,6 @@
 var problem = null;
+var solution = null;
+
 
 class Intersection {
   constructor(idx) {
@@ -42,6 +44,19 @@ class Problem {
 }
 
 
+class Solution {
+  constructor(problem) {
+    this.pb = problem;
+    this.cycles = {};
+    this.lights = {};
+  }
+
+  compute_lights() {
+
+  }
+}
+
+
 function parse_problem(file_content) {
   let lines = file_content.split("\n");
   let header = lines[0].split(" ");
@@ -61,13 +76,13 @@ function parse_problem(file_content) {
   }
 
   console.log(problem);
-  console.log("--- problem loaded ---")
+  console.log("--- problem loaded ---");
 
   return problem;
 }
 
 
-function problem_select() {
+function problem_select(callback=()=>{}) {
   let filename = this.value;
   if (filename == "")
     return;
@@ -76,6 +91,58 @@ function problem_select() {
   fetch("/data/" + filename)
   .then(response => response.text())
   .then(txt => parse_problem(txt))
-  .then(pb => problem = pb);
+  .then(pb => problem = pb)
+  .then(()=>{callback();});
 }
 document.getElementById("problem").onchange = problem_select;
+
+
+let problem_names = ["a.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f.txt"];
+function parse_solution(file_content) {
+  // if 
+  let sol = new Solution(problem);
+
+  let lines = file_content.split("\n");
+  let intersection_used = Number(lines[0]);
+  for (let idx=1 ; idx<lines.length ; idx++) {
+    let inter_idx = Number(lines[idx++]);
+    let nb_traffic_lights = Number(lines[idx++]);
+    let cycle = [];
+    for (let tl_idx=0 ; tl_idx<nb_traffic_lights ; tl_idx++, idx++) {
+      pair = lines[idx].split(" ");
+      pair[1] = Number(pair[1]);
+      cycle.push(pair);
+    }
+    sol.cycles[inter_idx] = cycle;
+  }
+
+  solution = sol;
+  console.log(sol);
+  console.log("--- Solution loaded ---")
+  return sol;
+}
+
+function solution_upload(event) {
+  let file = event.target.files[0];
+
+  if (problem == null) {
+    for (let pb_name of problem_names) {
+      if (file.name.startsWith(pb_name)) {
+        let pb_select = document.getElementById("problem");
+        pb_select.value = pb_name;
+        pb_select.onchange(()=>{solution_upload(event)});
+        return;
+      }
+    }
+    alert("A problem must be loaded first");
+    return;
+  }
+  console.log("loading " + file.name);
+
+  var reader = new FileReader();
+  reader.onload = (evt) => {parse_solution(evt.target.result);};
+  reader.readAsText(file);
+}
+document.getElementById("solution").onchange = solution_upload;
+
+
