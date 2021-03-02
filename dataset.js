9f30b2f3ee4d53b4a984b47e90c82a4ea1bf695b
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-03-02 20:53:45 +0100

    problem loading ok

--- dataset.js
+++ dataset.js
@@ -3,27 +3,65 @@ var problem = null;
 class Intersection {
   constructor(idx) {
     this.idx = idx;
-    this.input_streets = {};
-    this.output_streets = {};
+    this.input_streets = [];
+    this.output_streets = [];
+  }
+}
+
+
+class Street {
+  constructor(from, to, name, time) {
+    this.from = Number(from);
+    this.to = Number(to);
+    this.name = name;
+    this.time = Number(time);
+  }
+}
+
+class Car {
+  constructor(streets) {
+    this.streets = streets;
   }
 }
 
 
 class Problem {
   constructor(d, i, s, v, f) {
-    this.D = d;
-    this.I = i;
-    this.S = s;
-    this.V = v;
-    this.F = f;
+    this.D = Number(d);
+    this.I = Number(i);
+    this.S = Number(s);
+    this.V = Number(v);
+    this.F = Number(f);
+
+    this.intersections = {};
+    for (let i=0 ; i<this.I ; i++)
+      this.intersections[i] = new Intersection(i);
+    this.streets = {};
+    this.cars = [];
   }
 }
 
 
 function parse_problem(file_content) {
   let lines = file_content.split("\n");
-  let header = lines[0].split(" ").map(Number);
+  let header = lines[0].split(" ");
   problem = new Problem(...header);
+
+  let street_lines = lines.slice(1, problem.S+1);
+  for (let line of street_lines) {
+    let street = new Street(...line.split(" "));
+    problem.streets[street.name] = street;
+    problem.intersections[street.from].output_streets.push(street);
+    problem.intersections[street.to].input_streets.push(street);
+  }
+
+  let car_lines = lines.slice(problem.S+1, problem.S+1+problem.V);
+  for (let line of car_lines) {
+    problem.cars.push(new Car(line.split(" ").slice(1)));
+  }
+
+  console.log(problem);
+  console.log("--- problem loaded ---")
 
   return problem;
 }
